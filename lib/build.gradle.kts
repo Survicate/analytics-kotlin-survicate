834218953be3432eef61d7fc24c221fa4899e9af
Dariusz Konieczko <dariusz@survicate.com>
2025-12-30 09:23:33 +0100

    Release/6.5.2
    Update Survicate SDK, Gradle and other dependencies

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
 }
 
-ext.set("lib_version", "6.5.1")
+ext.set("lib_version", "6.5.2")
 
 android {
     namespace = "com.segment.analytics.kotlin.destinations.survicate"
