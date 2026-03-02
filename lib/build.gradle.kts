3382de43315f2d90df5f9385df1f3eebe4d10261
Dariusz Konieczko <148064035+dariusz-survicate@users.noreply.github.com>
2026-03-02 13:52:14 +0100

    Release/7.2.0 (#23)

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
 }
 
-ext.set("lib_version", "7.1.0")
+ext.set("lib_version", "7.2.0")
 
 android {
     namespace = "com.segment.analytics.kotlin.destinations.survicate"
